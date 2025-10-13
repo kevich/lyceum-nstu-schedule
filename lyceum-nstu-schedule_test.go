228858ddcd8fcbb554a45b53a5a057151912af45
Andrew Kevich <kevich@xiag.ch>
2025-10-13 22:39:40 +0700

    refactor TestHandler to prepare for sevaral cases

--- lyceum-nstu-schedule_test.go
+++ lyceum-nstu-schedule_test.go
@@ -11,16 +11,32 @@ import (
 )
 
 func TestHandler(t *testing.T) {
-	jsonTextBytes, err := os.ReadFile("./test/data/requests/empty_first_request.json")
-	assert.NoError(t, err, "Error reading local file")
-	var event domain.Event
-	err = json.Unmarshal(jsonTextBytes, &event)
-	assert.NoError(t, err, "failed parsing json %v")
-	var response *domain.Response
-	response, err = Handler(context.Background(), event)
-	if response != nil {
-		assert.Equal(t, "Привет, я могу рассказать расписание инженерного лицея НГТУ. Расписание какого класса и в какой день вас интересует?", response.Response.Text)
-	} else {
-		t.Errorf("Response should be returned")
+	tests := []struct {
+		name             string
+		inputJsonFile    string
+		expectedResponse string
+	}{
+		{
+			name:             "empty first request",
+			inputJsonFile:    "./test/data/requests/empty_first_request.json",
+			expectedResponse: "Привет, я могу рассказать расписание инженерного лицея НГТУ. Расписание какого класса и в какой день вас интересует?",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			jsonTextBytes, err := os.ReadFile(test.inputJsonFile)
+			assert.NoError(t, err, "Error reading local file")
+			var event domain.Event
+			err = json.Unmarshal(jsonTextBytes, &event)
+			assert.NoError(t, err, "failed parsing json %v")
+			var response *domain.Response
+			response, err = Handler(context.Background(), event)
+			if response != nil {
+				assert.Equal(t, test.expectedResponse, response.Response.Text)
+			} else {
+				t.Errorf("Response should be returned")
+			}
+		})
 	}
 }
